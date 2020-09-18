38fcbb15f2aa865e576c409ea8618749744ef0c9
Chief Keef <56800387+batiscuff@users.noreply.github.com>
2020-09-19 00:04:32 +0300

    Add files via upload

---
+++ tools/check_input.py
@@ -0,0 +1,42 @@
+import re
+
+
+class CheckInput:
+    def verification_phone(self, phone: str) -> str:
+        self.phone = phone
+        try:
+            phone = re.sub("[^0-9]", "", phone)  # оставляет только цифры
+            if phone.startswith("0") or phone.startswith("+", 1):
+                phone = "38" + phone
+                if len(phone) != 12:  # 380961115522
+                    print("\033[1;31m" + "Слишком много символов!")
+                    exit()
+                else:
+                    return phone
+            elif phone.startswith("380") and len(phone) == 12:
+                return phone
+            elif phone == "" or phone == " ":
+                print("\033[1;31m" + "Номер введён некорректно!")
+                exit()
+            else:
+                print("\033[1;31m" + "Неправильное кол-во символов!")
+                exit()
+        except Exception:
+            print("\033[1;31m" + "Номер введён некорректно!")
+            exit()
+
+    def verification_cycles(self, cycles: str) -> int:
+        try:
+            self.cycles = cycles
+            cycles = re.sub("[^0-9]", "", cycles)
+            return int(cycles)
+        except ValueError:
+            print("\033[1;31m" + "Неправильное кол-во циклов!")
+            exit()
+
+
+if __name__ == "__main__":
+    phone = input("Введите номер телефона: ")
+    cycles = input("Введите колличество циклов: ")
+    print(CheckInput().verification_phone(phone))
+    print(CheckInput().verification_cycles(cycles))
