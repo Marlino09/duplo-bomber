9a963e5acd88ff27660d9bb7d186e5a593767ee4
Chief Keef <56800387+batiscuff@users.noreply.github.com>
2020-09-28 17:03:55 +0300

    Update duplo_spam.py

--- duplo_spam.py
+++ duplo_spam.py
@@ -5,8 +5,11 @@ from send_requests import *
 import os
 
 
+clear = lambda: os.system("cls") if os.sys.platform == "win32" else os.system("clear")
+
+
 def main():
-    os.system("clear")
+    clear()
     print(banner, f"{replace}Введите номер телефона:{clear}", sep="\n")
     phone = input(cursor)
     phone = CheckInput().verification_phone(phone)
@@ -14,13 +17,13 @@ def main():
     print(f"{replace}Введите колличество циклов:{clear}")
     count = input(cursor)
     count = CheckInput().verification_cycles(count)
-    os.system("clear")
+    clear()
     print(banner)
     print(
         "\033[3;32m*Если вы ввели больше 10 циклов, то после 5-го скорость спама уменьшится\033[0m"
     )
     send_requests(phone, count)
-    os.system("clear")
+    clear()
     print(
         "\033[1;32mГотово!",
         f"Телефон: \033[35m{phone}\033[1;32m",
